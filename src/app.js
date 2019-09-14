06ded42f32e324b32573a8dfe1e8e576262a5756
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-15 06:35:00 +0800

    handlebars setup done

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const publicDirectory = path.join(__dirname, "../public");
+
+app.set("view engine", "hbs"); // handlebars setup
+app.use(express.static(publicDirectory));
 
 // 2 arguments required: (i) route, and (ii) callback function
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("This is an about page.")
+  res.render("index", {
+    title: "Handlebars title",
+    name: "Joyce"
+  });
 });
 
 app.get("/weather", (req, res) => {
-  res.send("<h1>This is a title.</h1>");
+  res.send({
+    location: "Hong Kong",
+    forecast: "It is sunny."
+  });
 });
 
 // Starts up server
